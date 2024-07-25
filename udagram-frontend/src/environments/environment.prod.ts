6a4bb6d3f6c29fa6a544928a46a56182b5b9caab
Vu Hoang <hoangvh123456@gmail.com>
2024-07-25 23:50:05 +0900

    commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://B77877284C6425BBC581B0F713E6FA64.gr7.us-east-1.eks.amazonaws.com:8080/api/v0'
 };
 
 /*
